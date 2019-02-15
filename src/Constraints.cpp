6fdbf26edb02b623b6efacf332c696e25b6fb98b
Arjan van der Velde <vandervelde.ag@gmail.com>
2019-02-15 14:20:35 -0500

    parse constraints using regexp

--- src/Constraints.cpp
+++ src/Constraints.cpp
@@ -1,6 +1,7 @@
 #include "Constraints.hpp"
 #include "Utils.hpp"
 #include <fstream>
+#include <regex>
 
 namespace zdock {
 
@@ -27,6 +28,35 @@ Constraints::Constraints(const std::string &filename)
 
 const std::vector<Constraint> &Constraints::constraints() const {
   return cons_;
+}
+
+std::istream &operator>>(std::istream &s, Constraint &c) {
+  const std::regex r("^([0-9]+)\\s+([A-Z0-9]+)\\s+([A-Z0-9]+)\\s+(.)\\s+([0-9]+"
+                     ")\\s+([0-9]+)\\s+([A-Z0-9]+)\\s+([A-Z0-9]+)\\s+(.)\\s+(["
+                     "0-9]+)\\s+([0-9.]+)(\\s+(MIN|MAX))?$");
+  std::smatch cm;
+  std::string line;
+  std::string ctype;
+  if (std::getline(s, line)) {
+    if (std::regex_match(line, cm, r)) {
+      c.recCoord.serialNum = std::stoi(cm[1]);
+      c.recCoord.atomName = cm[2];
+      c.recCoord.resName = cm[3];
+      c.recCoord.chain = cm[4].str().c_str()[0];
+      c.recCoord.resNum = std::stoi(cm[5]);
+      c.ligCoord.serialNum = std::stoi(cm[6]);
+      c.ligCoord.atomName = cm[7];
+      c.ligCoord.resName = cm[8];
+      c.ligCoord.chain = cm[9].str().c_str()[0];
+      c.ligCoord.resNum = std::stoi(cm[10]);
+      c.distance = std::stod(cm[11]);
+      c.constraintType =
+          ("MIN" == std::string(cm[13]) ? Constraint::MIN : Constraint::MAX);
+    } else {
+      throw ConstraintException("Constraint format error");
+    }
+  }
+  return s;
 }
 
 } // namespace zdock
